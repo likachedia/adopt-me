247c67e961682cbd1fa573feb4d80cf522c60208
likachedia <likachedia@gmail.com>
2024-07-02 16:25:07 +0400

    add onSubmit request

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -26,7 +26,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form action="">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
