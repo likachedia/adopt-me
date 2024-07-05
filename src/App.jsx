3a65b91e42996b58c5e7d5141f5fc1046944dbe9
likachedia <likachedia@gmail.com>
2024-07-05 15:39:33 +0400

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt Me!</h1>
-      <SearchParams/>
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
-{/* <Pet name="Luna" animal="dog" breed="Havanese" />
-<Pet name="Pepper" animal="bird" breed="Cockatiel" />
-<Pet name="Doink" animal="cat" breed="Mix" /> */}
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
+root.render(<App />);
