ea82ddcc3dddf7d1f91c2edd52bbf3a3ab3575c5
likachedia <likachedia@gmail.com>
2024-07-11 22:23:02 +0400

    create carusel with class components

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,15 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPets from "./fetchPets";
+import Carousel from "./Carousel";
 
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPets);
 
-  if(results.isError){
-    return (
-        <h2>There is something wrong fetching pets details</h2>
-    )
+  if (results.isError) {
+    return <h2>There is something wrong fetching pets details</h2>;
   }
 
   if (results.isLoading) {
@@ -25,6 +24,7 @@ const Details = () => {
   return (
     <div className="details">
       <div>
+        <Carousel images={pet.images} />
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
         <button>Adopt {pet.name}</button>
